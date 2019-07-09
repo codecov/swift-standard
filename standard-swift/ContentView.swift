c7ae01c17da95bf0516f793a65b9af20d16e84c9
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-07-09 14:31:52 -0700

    Initial Commit

---
+++ standard-swift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  standard-swift
+//
+//  Created by Ibrahim on 7/9/19.
+//  Copyright © 2019 Ibrahim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
