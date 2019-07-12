788dfff2d9dc32d5b282adb8fd564bfdce729693
“ibrahim0814” <ibrahim.a.ali@vanderbilt.edu>
2019-07-12 02:39:31 -0700

    correct coverage percentage for api call verification

--- request.py
+++ request.py
@@ -7,19 +7,22 @@ payload = {'token': os.environ['API_KEY']}
 
 link = 'https://codecov.io/api/gh/ibrahim0814/SwiftStandard-Draft'
 
+print("Waiting 60 seconds for report to upload before pinging API...")
+
+#night night
 time.sleep(60)
 
+print("Pinging Codecov's API..")
 #get latest coverage data
 all_data = requests.get(link, params=payload).json()
-
 commit_data = all_data['commits'][0]
-
 coverage_percentage = commit_data['totals']['c']
 
-#result should return 83.33333 as its coverage metric
-if(coverage_percentage == '83.33333'): 
-    print("success!")
+print("Ensuring coverage percentage is accurate...")
+#result should return 82.35294 as its coverage metric
+if(coverage_percentage == '82.35294'): 
+    print("Success! Codecov's API returned the correct coverage percentage")
     exit(0)
 else:
-    print("something is wrong")
+    print("Whoops, something is wrong D: Codecov did not return the correct coverage percentage. Coverage percentage should be 82.35294 but Codecov returned "+coverage_percentage)
     exit(1)
