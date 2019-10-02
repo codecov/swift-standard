00b839659d50de5163b65ed2fa4ceabf040d2ab4
Felipe <felipe@codecov.io>
2019-10-02 15:42:11 -0700

    Testtt

--- standard-swift/Index.swift
+++ standard-swift/Index.swift
@@ -12,11 +12,13 @@ class Index {
     
     //Partially be covered by unit tests
     func uncovered_if(a: Bool = true) -> Bool {
+        var b = true
         if(a == true){
-            return false
+            b = false
         }else{
             return true
         }
+        return b
     }
     
     //Will be fully covered by unit tests
