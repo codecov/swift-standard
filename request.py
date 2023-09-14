a5ed79644da14871c3528557e932ca00feecc139
Tom Hu <thomas.hu@sentry.io>
2023-09-14 13:28:43 -0700

    fix: get real logs from request.py

--- request.py
+++ request.py
@@ -22,8 +22,11 @@ coverage_percentage = commit_data['totals']['coverage']
 print("Ensuring coverage percentage is accurate...")
 #result should return 86.66667 as its coverage metric
 if(str(coverage_percentage) == os.environ['CORRECT_COVERAGE']):
-    print("Success! Codecov's API returned the correct coverage percentage, "+ os.environ['CORRECT_COVERAGE'])
+    print(
+        "Success! Codecov's API returned the correct coverage percentage, ",
+        os.environ['CORRECT_COVERAGE']
+    )
     exit(0)
 else:
-    print("Whoops, something is wrong D: Codecov did not return the correct coverage percentage. Coverage percentage should be "+os.environ['CORRECT_COVERAGE']+" but Codecov returned "+coverage_percentage)
+    print("Whoops, something is wrong D: Codecov did not return the correct coverage percentage. Coverage percentage should be ",os.environ['CORRECT_COVERAGE']," but Codecov returned ",coverage_percentage)
     exit(1)
